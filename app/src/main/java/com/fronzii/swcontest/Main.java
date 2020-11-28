71d3e2eb0dc806e9c32cae75304247482b0cb0f2
fronzii <xyx_1208@outlook.com>
2020-11-28 20:33:59 +0800

    update

--- app/src/main/java/com/fronzii/swcontest/Main.java
+++ app/src/main/java/com/fronzii/swcontest/Main.java
@@ -1,12 +1,16 @@
 package com.fronzii.swcontest;
 
+import android.app.Application;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageInfo;
 
 import com.fronzii.swcontest.Collect.Collect;
+import com.fronzii.swcontest.Util.SWlog;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class Main implements IXposedHookLoadPackage{
@@ -16,11 +20,23 @@ public class Main implements IXposedHookLoadPackage{
     public static Collect collect = new Collect();
 
     public void initconfig(){
-        
+        // TODO
     }
 
     @Override
-    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        if(MainActivity.isLaunch){
+            if(MainActivity.appName.contains(loadPackageParam.processName)){
+                XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class,
+                        new XC_MethodHook() {
+                            @Override
+                            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                                ClassLoader cl =((Context)param.args[0]).getClassLoader();
+                                SWlog.d("PackageName:" + loadPackageParam.processName);
 
+                            }
+                        });
+            }
+        }
     }
 }
--- app/src/main/java/com/fronzii/swcontest/MainActivity.java
+++ app/src/main/java/com/fronzii/swcontest/MainActivity.java
@@ -2,21 +2,26 @@ package com.fronzii.swcontest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    static boolean launchFlag = false;
-    static boolean scanFlag = false;
-    static List<PackageInfo> appListInfo = null;
+    static boolean isLaunch = false;
+    static boolean isScanning = false;
+    static List<String> appName = new ArrayList<String>();
+    static Map<String, Context> contextMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        launchFlag = true;
+        isLaunch = true;
         getAppList();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -25,6 +30,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void getAppList(){
         PackageManager packageManager = this.getPackageManager();
-        appListInfo = packageManager.getInstalledPackages(0);
+        List<PackageInfo> appListInfo = packageManager.getInstalledPackages(0);
+        for(PackageInfo each : appListInfo){
+            appName.add(each.packageName);
+        }
+        for(String each : appName){
+            contextMap.put(each, null);
+        }
     }
 }

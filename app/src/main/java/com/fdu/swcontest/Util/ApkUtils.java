531b244bbb544377e48738376d73d3ca70d05a85
fronzii <xyx_1208@outlook.com>
2020-12-11 15:09:00 +0800

    update Apkutils

--- app/src/main/java/com/fdu/swcontest/Util/ApkUtils.java
+++ app/src/main/java/com/fdu/swcontest/Util/ApkUtils.java
@@ -1,9 +1,14 @@
 package com.fdu.swcontest.Util;
 
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 import android.widget.ListAdapter;
+
+import com.fdu.swcontest.Hooks.AbstractHook;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,9 +18,12 @@ public class ApkUtils {
     public static class SWAppInfo{
         public Drawable icon;
         public String appName;
+        public Boolean isMal;
     }
 
-    public static List<SWAppInfo> scan(PackageManager packageManager){
+    public static List<SWAppInfo> scan(Context context){
+        PackageManager packageManager = context.getPackageManager();
+        ContentResolver contentResolver = context.getContentResolver();
         List<SWAppInfo> appInfos = new ArrayList<SWAppInfo>();
         try{
             List<PackageInfo> packageInfos = packageManager.getInstalledPackages(0);
@@ -27,6 +35,16 @@ public class ApkUtils {
                     continue;
                 }
                 swAppInfo.icon = packageInfo.applicationInfo.loadIcon(packageManager);
+                Cursor cursor = contentResolver.query(AbstractHook.uri_sequence, new String[]{"isMal"},
+                        "packagename=?", new String[]{packageInfo.packageName}, null);
+                assert cursor != null;
+                if(cursor.isAfterLast()){
+                    swAppInfo.isMal = false;
+                }else {
+                    cursor.moveToNext();
+                    swAppInfo.isMal = cursor.getInt(0) > 0;
+                }
+                cursor.close();
                 appInfos.add(swAppInfo);
             }
         }catch(Exception e){
